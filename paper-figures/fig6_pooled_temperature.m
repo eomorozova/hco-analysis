5cfd96640d528a57ce3488b8284bc45d7710d377
Ekaterina Morozova <morozova.e.o@gmail.com>
2021-08-24 17:45:02 -0400

    Added bar plots for cycle frequency

--- paper-figures/fig6_pooled_temperature.m
+++ paper-figures/fig6_pooled_temperature.m
@@ -28,12 +28,21 @@ for i=[ie,ir]
 
     % change in characteristics with temperature
 
-    dFR(k) = diff(FR(k,:));
+    dFR(k) = diff(FR(k,:)); 
     ddc(k) = diff(dc(k,:));
     dA(k) = diff(A(k,:));
     dSpkFR(k) = diff(SpkFR(k,:));
     dNspks(k) = diff(Nspks(k,:));
     dERQ1(k) = diff(ERQ1(k,:));
+
+    % percent change in characteristics with temperature
+
+    dperFR(k) = dFR(k)/FR(k,1)*100; 
+    dperdc(k) = ddc(k)/dc(k,1)*100;
+    dperA(k) = ddc(k)/dc(k,1)*100;
+    dperSpkFR(k) = dSpkFR(k)/SpkFR(k,1)*100;
+    dperNspks(k) = dNspks(k)/Nspks(k,1)*100;
+    dperERQ1(k) = dERQ1(k)/ERQ1(k,1)*100;
     
     k=k+1;
 end
@@ -44,3 +53,35 @@ ir=find(data.mode=="release");
 iq1=find(data.Q10==11);
 iq2g=find(data.Q10==21);
 iq2=find(data.Q10==22);
+
+%% plot percent change in frequency
+
+color = {[0.36,0.42,0.6],[0.6,0.4,1],[0.36,0.23,0.6],[0.8, 0.6, 0],[0.8, 0.3, 0],[0.5, 0, 0]};
+symbol = {'o','o','o','s','s','s'};
+clf; subplot(2,4,1)
+
+meanFR=[median(dperFR(intersect(ie,iq1))),median(dperFR(intersect(ie,iq2g))),median(dperFR(intersect(ie,iq2))),...
+median(dperFR(intersect(ir,iq1))),median(dperFR(intersect(ir,iq2g))),median(dperFR(intersect(ir,iq2))),];
+
+errFR=[std(dperFR(intersect(ie,iq1))),std(dperFR(intersect(ie,iq2g))),std(dperFR(intersect(ie,iq2))),...
+std(dperFR(intersect(ir,iq1))),std(dperFR(intersect(ir,iq2g))),std(dperFR(intersect(ir,iq2))),];
+
+display.plot_barerror(meanFR,errFR,'color',color); hold on
+
+groupIdx = [ones(size(intersect(ie,iq1)))'; 2*ones(size(intersect(ie,iq2g)))';...
+ 3*ones(size(intersect(ie,iq2)))'; 4*ones(size(intersect(ir,iq1)))';...
+ 5*ones(size(intersect(ir,iq2g)))'; 6*ones(size(intersect(ir,iq2)))'];
+
+display.plot_scatter(dperFR, groupIdx,'color',{'k','k','k','k','k','k'},'symbol',symbol,'markersize',50)
+
+h=display.plothorzline(0); set(h,'color','k','linewidth',1)
+
+xlim([0.5 6.5]); ylim([-60 180])
+xticks([1,2,3,4,5,6]);
+xticklabels({'q_{10}=1','q_{10}=2 gs','q_{10}=2','q_{10}=1','q_{10}=2 gs','q_{10}=2'});
+ylabel('% \Delta in cycle frequency');
+title('Percent change in cycle frequency')
+set(gca,'Fontsize',14,'Fontname','Arial')
+box off; axis square
+set(groot,{'DefaultAxesXColor','DefaultAxesYColor','DefaultAxesZColor'},{'k','k','k'})
+
