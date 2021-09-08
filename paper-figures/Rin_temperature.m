abfc7658d06af094165ca6f659f4e37184652113
Ekaterina Morozova <morozova.e.o@gmail.com>
2021-09-08 17:26:21 -0400

    Calculate input resistance

--- paper-figures/Rin_temperature.m
+++ paper-figures/Rin_temperature.m
@@ -5,30 +5,31 @@ load('C:\Users\moroz\Documents\code\hco_analysis\data\GM_FI_Rin.mat')
 %% calculate Rin & FIs
 
 data10=[]; data20=[];
+Rin10=[]; Rin20=[];
 
-for j=1:numel(data) % experiments
-    for i=1:size(data.V{j},2) % neurons
-        data10{j}=fi(data.V{j}{1,i},data.I{j}{1,i},Fs); % 10oC
-        data20{j}=fi(data.V{j}{2,i},data.I{j}{2,i},Fs); % 20oC
-    end
+for j=1:numel(data.V) % experiments
+    
+    data10{j}=fi(data.V{j}(1,:),data.I{j}(1,:),Fs); % 10oC
+    data20{j}=fi(data.V{j}(2,:),data.I{j}(2,:),Fs); % 20oC
+    
+    Rin10 = [Rin10, data10{j}.Rin1];
+    Rin20 = [Rin20, data20{j}.Rin1];
+    
 end
 
 %% Wilcoxon signed rank-sum test
-[p,h] = signrank(Rin20,Rin20)
 
-% plot input resistance @ 10oC and 20oC
+[p,h] = signrank(Rin10,Rin20)
+
+%% plot input resistance @ 10oC and 20oC
 
 addpath('C:\Users\moroz\Dropbox\ml\plotting')
 clf, plot([1 2],[Rin10; Rin20],'.-','markersize',20,'linewidth',1,'color','k'), hold on
-xlim([0.9 2.1]); ylim([4 16])
+xlim([0.9 2.1]); ylim([0 16])
 xticks([1 2]); xticklabels({'10^oC','20^oC'});
     ylabel('Input resistance, MOhms'); box off
 set(gca,'Fontsize',16,'FontName','Myriad Pro')
 display.sigstar([1,2],p)
 text(1.3,15.5,['n.s. (p=',num2str(round(p,4)),')'],'fontsize',16,'FontName','Myriad Pro')
-text(1.9,15.5,'N=9','fontsize',14,'FontName','Arial')
+text(1.9,15.5,'N=10','fontsize',14,'FontName','Arial')
 set(gca,'Fontsize',16,'FontName','Arial')
-
-
-
-
