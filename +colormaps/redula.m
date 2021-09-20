1e094c1188450a2247d0212440571658eebbd109
Ekaterina Morozova <morozova.e.o@gmail.com>
2021-09-20 13:51:06 -0400

    Colormaps

---
+++ +colormaps/redula.m
@@ -0,0 +1,21 @@
+% like parula, but becomes red and high values instead
+% of yellow
+
+function C = redula(N)
+
+
+
+if nargin == 0
+	N = 100;
+else
+	assert(isnumeric(N) && isscalar(N) && isreal(N) ,'N must be a +ve integer');
+	N = round(N);
+end
+
+
+C = parula(N);
+
+idx = floor(N*.8);
+
+
+C(idx+1:end,2) = linspace(1,0,N-idx).*C(idx+1:end,2)';
