aa84e666bf9255cca8d357b7272a41a6ae996e3b
Ekaterina Morozova <morozova.e.o@gmail.com>
2021-11-30 17:26:40 -0500

    Calculate half-center oscillator features

--- +analysis/hco_stat.m
+++ +analysis/hco_stat.m
@@ -7,53 +7,42 @@ function [hcostat]=hco_stat(V, Fs)
 %
 % Outputs:
 % hcostat
-%   .T _mean    mean cycle periods based on the spikes
-%   .T_std standard deviation of HCO cycle periods  based on the spikes
-%   .T HCO cycle periods based on spikes
-%   .T1 _mean mean HCO cycle periods based on the slow wave
-%   .T1_std standard deviation of HCO cycle periods  based on the slow wave
-%   .T1 HCO cycle periods based on the slow wave
-%   .nSpks_mean mean number of spikes per burst 
-%   .nSpks_std standard deviation of the number of spikes per burst based
-%   .SpkFreq_mean mean frequency of spikes withn the burst
-%   .SpksFreq_std standard deviation of the frequency of spikes within the
-%   .dc duty cycles based on the spikes 
-%   .dc_mean mean duty cycle based on the spikes
-%   .dc_std standard deviation of the duty cycle based on the spikes
-%   .DC duty cycles based on the slow wave 
-%   .DC_mean mean duty cycle based on the slow wave
-%   .DC_std standard deviation of the duty cycle based on the slow wave
-%   .A amplitude of the slow wave
-%   .mean_pks  mean slow wave peak
-%   .mean_dip mean slow wave trough
-%   .st spike times
+%   .T _mean        mean cycle period (based on spikes)
+%   .T_std          standard deviation of the cycle period (based on spikes)
+%   .T              cycle periods (based on spikes)
+%   .T1 _mean       mean cycle period (based on slow-wave)
+%   .T1_std         standard deviation of the cycle period (based on slow-wave)
+%   .T1             cycle periods (based on slow-wave)
+%   .nSpks_mean     mean number of spikes per burst 
+%   .nSpks_std      standard deviation of the number of spikes per burst
+%   .SpkFreq_mean   mean frequency of spikes within the burst
+%   .SpksFreq_std   standard deviation of the frequency of spikes within the
+%   .dc             duty cycles (based on spikes) 
+%   .dc_mean        mean duty cycle (based on spikes)
+%   .dc_std         standard deviation of the duty cycle (based on spikes)
+%   .DC             duty cycles (based on slow-wave) 
+%   .DC_mean        mean duty cycle (based on slow-wave)
+%   .DC_std         standard deviation of the duty cycle (based on slow-wave)
+%   .A              amplitude of the slow-wave
+%   .mean_pks       mean values of the slow-wave peaks
+%   .mean_dip       mean values of the slow-wave troughs
+%   .st             spike times
 
 if nargin<2
     Fs=10000; % sampling frequency (Hz)
 end
-%% calculates burst period of HCO, number of spikes per burst and spike frequency
-%i=find(gh1==350); j=find(gsyn1==450);
-%V=Vhco4{i,j}(2,1:end);
-%V=Vhco{1}{45,75}(1:end);
-%V=maps.Vhco{jj}{1}{75,90}(1:end);
-%V=Vseg{1}{9};
-%V=Vhco{1}{4}{31}(1,5*Fs(1):end);
+%% calculates burst period, number of spikes per burst and spike frequency of HCO
  
-%[st_v,st]=findpeaks(smooth(V,10),'MinPeakProminence',2,'MinPeakHeight',-38);
 [st_v,st]=findpeaks(smooth(V,10),'MinPeakProminence',1.5,'MinPeakHeight',-38);
 
 %burststat=detect_bursts(st'/Fs,mean(diff(st))/Fs,mean(diff(st))/Fs+1.2,1); % for most experiments
 %burststat=detect_bursts(st'/Fs,mean(diff(st))/Fs,mean(diff(st))/Fs+0.75,1); % for 936_122
-%burststat=detect_bursts(st'/Fs,mean(diff(st))/Fs,mean(diff(st))/Fs+0.5,1);
-%burststat=detect_bursts(st'/Fs,mean(diff(st))/Fs,mean(diff(st))/Fs+0.4,1);
-burststat=analysis.detect_bursts(st'/Fs,mean(diff(st))/Fs,mean(diff(st))/Fs+0.5,1); % 951_060
 %burststat=detect_bursts(st'/Fs,mean(diff(st))/Fs,mean(diff(st))/Fs+1.5,1); % for 936_074
-%burststat=detect_bursts(st'/Fs,0.9,0.9,1);
 %burststat=detect_bursts(st'/Fs,0.55,0.55,1); % 936_102 release
+
+burststat=analysis.detect_bursts(st'/Fs,mean(diff(st))/Fs,mean(diff(st))/Fs+0.5,1); % 951_060
+
 T=diff(burststat.center);
-%if isempty(st)==1
-%    st=0;
-%end
 
 if length(burststat.center)>2
     T_mean=mean(diff(burststat.center)); T_std=std(diff(burststat.center));
@@ -70,75 +59,73 @@ if length(burststat.center)>2
     end
     dc_mean = mean(dc); dc_std = std(dc);
 else
-    T_mean=0; nSpks_mean=0; SpkFreq_mean=0; SpkFreq_median=0; dc_mean=0; dc=0;
-    T_std=0; nSpks_std=0; SpkFreq_std=0; dc_std=0;
+    %T_mean=0; nSpks_mean=0; SpkFreq_mean=0; SpkFreq_median=0; dc_mean=0; dc=0;
+    %T_std=0; nSpks_std=0; SpkFreq_std=0; dc_std=0;
+    
+    T_mean=NaN; nSpks_mean=NaN; SpkFreq_mean=NaN; SpkFreq_median=NaN; dc_mean=NaN; dc=NaN;
+    T_std=NaN; nSpks_std=NaN; SpkFreq_std=NaN; dc_std=NaN;
 end
+
 %uncomment and run this subsection to see how well bursts are identified
 %figure(2),clf, plot(1/Fs:1/Fs:length(V)/Fs,V),
 %hold on, plot(burststat.center, repmat(-45, 1, length(burststat.center)),'.','markersize',20)
 %hold on, plot(st/Fs,st_v,'.','markersize',15,'color','k')
 
-%% filter data, calculate the amplitude of the slow wave and the frequency by threholding (1st neuron)
-%V=maps.Vhco{jj}{1}{75,90}(1:end);
-%V=Vhco{1}{1}{12}(2,:);
+%% filter data, calculate the amplitude of the slow wave and oscillation frequency by threholding (1st neuron)
+
 [b,a]=butter(4,1/(Fs/2));
 Vfilt = filter(b,a,V); % filter the data to frequency ~2 times higher than desired frequency
-Vfilt_smooth=smooth(Vfilt(1:1:end),1000);
-%Vfilt_smooth=smooth(Vfilt(1:1:end),100); % if Fs=1000 Hz
-%[pks,lpks]=findpeaks(Vfilt_smooth(Fs:end),'MinPeakDistance',Fs/10*2,'MinPeakProminence',3*std(Vfilt_smooth(Fs:end)));
-%[dips,ldips]=findpeaks(-Vfilt_smooth(Fs:end),'MinPeakDistance',Fs/10*2,'MinPeakProminence',3*std(Vfilt_smooth(Fs:end)));
+Vfilt_smooth=smooth(Vfilt(1:1:end),Fs/10);
 
-%deltaV=3; % for most experiments
 %deltaV=1.8; % for 936_122 (release)
-deltaV=2.5; % for 936_074
+deltaV=2.5;
+
 [pks,lpks]=findpeaks(Vfilt_smooth(Fs:end),'MinPeakProminence',3,'MinPeakHeight',mean(Vfilt_smooth(Fs:end))+deltaV);
 [dips,ldips]=findpeaks(-Vfilt_smooth(Fs:end),'MinPeakProminence',3,'MinPeakHeight',mean(-Vfilt_smooth(Fs:end))+deltaV);
-if length(pks)>=2
-%if length(pks)>2 % 936_074
-mean_pks=median(pks); mean_dip=median(-dips);
-A=mean_pks - mean_dip; % slow wave amplitude
-Vth=(mean_pks + mean_dip)/2; 
-st_filt = analysis.crossings(smooth(Vfilt(1:1:end),1000)',Vth); st_filt=st_filt{1}./Fs; % in sec
-[~,st_filtdown] = analysis.crossings(smooth(Vfilt(1:1:end),1000)',Vth); st_filt_down=st_filtdown{1}./Fs; % in sec
 
-if length(st_filt)>=2; T1=diff(st_filt); else; T1=0; end
-T1_mean=mean(T1); T1_std=std(T1);
-
-% based on the peaks of the slow-wave
-%if length(lpks)>=2; T1=diff(lpks)./Fs; else; T1=0; end
-%T1_mean=mean(T1); T1_std=std(T1);
-
-if length(st_filt)>=2
-%if length(st_filt)>2 % 936_074
-if st_filt_down(1)<st_filt(1)
-    if length(st_filt_down(2:end))<length(st_filt)
-        BurDur=st_filt_down(2:end)-st_filt(1:end-1); % burst duration based on slow wave
+%if numel(pks)>=2
+if numel(pks)>2
+    mean_pks=median(pks); mean_dip=median(-dips);
+    A=mean_pks - mean_dip; % slow wave amplitude
+    Vth=(mean_pks + mean_dip)/2;
+    
+    st_filt = analysis.crossings(smooth(Vfilt(1:1:end),Fs/10)',Vth); st_filt=st_filt{1}./Fs; % in sec
+    [~,st_filtdown] = analysis.crossings(smooth(Vfilt(1:1:end),Fs/10)',Vth); st_filt_down=st_filtdown{1}./Fs; % in sec
+        
+    %if numel(st_filt)>=2
+    if numel(st_filt)>2  && ~isempty(burststat.nSp)  
+        T1=diff(st_filt);
+        
+        if st_filt_down(1)<st_filt(1)
+            if length(st_filt_down(2:end))<length(st_filt)
+                BurDur=st_filt_down(2:end)-st_filt(1:end-1); % burst duration based on the slow-wave
+            else
+                BurDur=st_filt_down(2:end)-st_filt; % burst duration based on the slow-wave
+            end
+        else
+            if length(st_filt)> length(st_filt_down)
+                BurDur=st_filt_down-st_filt(1:end-1); % burst duration based on the slow-wave
+            else
+                BurDur=st_filt_down-st_filt; % burst duration based on the slow-wave
+            end
+        end
+        
+        if length(BurDur)>length(T1)
+            DC=BurDur(2:end-1)./T1(2:end); % duty cycle based on the slow wave
+        else
+            DC=BurDur(2:end)./T1(2:end); % duty cycle based on the slow wave
+        end
+        
+        T1_mean=mean(T1); T1_std=std(T1);
+        DC_mean = mean(DC); DC_std = std(DC);
     else
-        BurDur=st_filt_down(2:end)-st_filt; % burst duration based on slow wave
+        T1=NaN; T1_mean=NaN; T1_std=NaN;
+        DC=NaN; DC_mean=NaN; DC_std=NaN;
     end
+    
 else
-    if length(st_filt)> length(st_filt_down)
-        BurDur=st_filt_down-st_filt(1:end-1); % burst duration based on slow wave
-    else
-        BurDur=st_filt_down-st_filt; % burst duration based on slow wave
-    end
-end
-end
-
-if length(st_filt)>=2
-%if length(st_filt)>2 % 936_074
-    if length(BurDur)>length(T1)
-        DC=BurDur(2:end-1)./T1(2:end); % duty cycle based on the slow wave
-    else
-        DC=BurDur(2:end)./T1(2:end); % duty cycle based on the slow wave
-    end
-    DC_mean = mean(DC); DC_std = std(DC);
-else
-    DC=0; DC_mean=0;  DC_std=0;
-end
-
-else
-mean_pks=0; mean_dip=0; A=0; T1=0; T1_mean=0; T1_std=0; DC=0; DC_mean=0; DC_std=0;
+    %mean_pks=0; mean_dip=0; A=0; T1=0; T1_mean=0; T1_std=0; DC=0; DC_mean=0; DC_std=0;
+    mean_pks=NaN; mean_dip=NaN; A=NaN; T1=NaN; T1_mean=NaN; T1_std=NaN; DC=NaN; DC_mean=NaN; DC_std=NaN;
 end
 
 %uncomment to check period calculation
