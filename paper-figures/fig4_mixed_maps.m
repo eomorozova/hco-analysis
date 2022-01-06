9a6996035356586b6c87f778f0a1821442e19fbd
Ekaterina Morozova <morozova.e.o@gmail.com>
2022-01-05 19:52:05 -0500

    Added critera for identification of the oscillation mechan

--- paper-figures/fig4_mixed_maps.m
+++ paper-figures/fig4_mixed_maps.m
@@ -7,7 +7,7 @@ load('C:\Users\moroz\Documents\code\hco_analysis\data\data_fig4.mat')
 
 %hcostat=[]; ERQ=[];
 
-for jj = 3:5 %:numel(data) % experiment
+for jj = 5 %1:numel(data) % experiment
     jj
     for j = 1:numel(data{jj}.V) % map number
         for i = 1:length(data{jj}.V{j}) % (gh,gsyn) combination
@@ -29,15 +29,19 @@ for jj = 3:5 %:numel(data) % experiment
     end
 end
 
+% fix manually missing data point in one experiment
+hcostat{5}{4}{49}{1} = []; hcostat{5}{4}{49}{2} = [];
+ERQ{5}{4}(:,49) = NaN; 
+
 %% extract burst features
 
 FR =[]; A=[]; SpkFR=[]; Nspk=[]; DC=[]; dc=[];
 
 meanFR=[]; stfFR=[]; CVFR=[];
 
-for jj = 1:2 %1:numel(data) % experiment
+for jj = 1:1:numel(data) % experiment
     for j = 1:numel(data{jj}.V) % map number
-        for i = 1:length(data{jj}.V{j}) % (gh,gsyn) combination
+        for i = 1:numel(hcostat{jj}{j}) % (gh,gsyn) combination
             for ii = 1:2 % neuron number
                 if ~isempty(hcostat{jj}{j}{i}{ii})
                     FR{jj}{j}(ii,i) = 1./hcostat{jj}{j}{i}{ii}.T1_mean; % cycle frequency
@@ -49,7 +53,12 @@ for jj = 1:2 %1:numel(data) % experiment
 
                     % evaluate asymmetry between neurons
                     BurDur{jj}{j}{ii,i} = hcostat{jj}{j}{i}{ii}.BurDur; % based on slow-wave
-                    meanBurDur{jj}{j}(ii,i) =  mean(BurDur{jj}{j}{ii,i});
+                    
+                    if jj==1 && j==2 && i==31
+                        meanBurDur{jj}{j}(ii,i) =  mean(BurDur{jj}{j}{ii,i}(2:end))
+                    else
+                        meanBurDur{jj}{j}(ii,i) =  mean(BurDur{jj}{j}{ii,i});
+                    end
                     
                     BrstDur{jj}{j}{ii,i} =  hcostat{jj}{j}{i}{ii}.burststat.BuDur; % based on spikes
                     meanBrstDur{jj}{j}(ii,i) =  mean(BrstDur{jj}{j}{ii,i});
@@ -74,22 +83,23 @@ for jj = 1:2 %1:numel(data) % experiment
                     FR{jj}{j}(ii,i)=NaN; A{jj}{j}(ii,i)=NaN;
                     SpkFR{jj}{j}(ii,i)=NaN; Nspk{jj}{j}(ii,i)=NaN;
                     DC{jj}{j}(ii,i)=NaN; dc{jj}{j}(ii,i)=NaN;
-                                      
+                    meanBurDur{jj}{j}(ii,i)=NaN;  CVFR{jj}{j}(ii,i)=NaN;
                 end
             end
         end
     end
 end
 
+
 %% classify the network state (silent, spiking, half-center,..)
 
 networkstate=[]; state=[]; percentSingleSpikeBursts=[];
 
-for jj = 1:2 %numel(data) % experiment
+for jj = 1:numel(data) % experiment
     
     for j = 1:numel(data{jj}.V) % map
         
-        for i = 1:length(data{jj}.V{j}) % (gh,gsyn) combination
+        for i = 1:numel(hcostat{jj}{j}) % (gh,gsyn) combination
             
             if ~isempty(hcostat{jj}{j}{i}{1})==1
                 st1 = hcostat{jj}{j}{i}{1}.st/Fs(j); st2 = hcostat{jj}{j}{i}{2}.st/Fs(j); % spike times in s
@@ -137,8 +147,9 @@ for jj = 1:2 %numel(data) % experiment
                 else
                     
                     networkstate{jj}{j}(i) = 6; % misclassified
-                    
                 end
+            else              
+                networkstate{jj}{j}(i) = 1; %silent
             end
         end
         
@@ -148,7 +159,7 @@ end
 
  %% if identified state is not a half-center, make sure all the half-center characteristics are NaN
 
- for jj=1:2 % experiment
+ for jj=1:numel(data) % experiment
      for j=1:numel(state{jj}) % map number
          for i=1:length(networkstate{jj}{j}) % (gh,gsyn) combination
              if state{jj}{j}(i)~=5 % if not a half-center
@@ -169,7 +180,7 @@ end
 
 %% calculate mean characteristics across two neurons in a circuit
 
-for jj = 1:2 %:numel(data)
+for jj = 1:numel(data)
     for j = 1:numel(data{jj}.V)
         ERQall{jj}(j,:) = mean(ERQ{jj}{j});
         FRall{jj}(j,:) = mean(FR{jj}{j});
@@ -184,7 +195,7 @@ for jj = 1:2 %:numel(data)
         %reshape
         ERQmean{jj}{j} = reshape(ERQall{jj}(j,:),[7,7])';
         for i=1:2
-            ERQ1{jj}{j} = reshape(ERQ{jj}{j}(i,:),[7,7]);
+            ERQ1{jj}{j}{i} = reshape(ERQ{jj}{j}(i,:),[7,7]);
         end
         FRmean{jj}{j} = reshape(FRall{jj}(j,:),[7,7])';
         Amean{jj}{j} = reshape(Aall{jj}(j,:),[7,7])';
@@ -209,9 +220,12 @@ mindc = min(dcall{jj}(:)); maxdc = max(dcall{jj}(:));
 
 minCVFR = min(CVFRall{jj}(:)); maxCVFR = max(CVFRall{jj}(:)); 
 
-%% reproduce Figure 4 A-B
+%% reproduce Figure 4A
+
+% map
 clf
 jj=2; j=5;
+
 display.genimagesc(ERQmean{jj}{j},gH,gSyn);
 myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(myColorMap);
 if j ==5; h=colorbar; ylabel(h,'ERQ'); end
@@ -226,7 +240,25 @@ for i=1:10 % create grid
     line([0 1200], [150*i+75 150*i+75],'color',[0.5 0.5 0.5])
     line([150*i+75 150*i+75],[0 1200],'color',[0.5 0.5 0.5])
 end
-        
+
+%% plot example traces (Figure 4B)
+ii=[43,30,17,12,7]; k=1;
+for i=[1:5] % 5 traces
+    for n=1:2 % neuron
+    x=1/Fs(j):1/Fs(j):length(data{jj}.V{j}{ii(k)}(n,Fs(j)*5:end))/Fs(j);
+    subplot(4,5,1+5*(n-1)+(k-1))
+    plot(x,data{jj}.V{j}{ii(k)}(n,Fs(j)*5:end),'color','k','linewidth',1)
+    %Vmean = mean(data{jj}.V{j}{ii(k)}(n,Fs(j)*5:end));
+    %hold on, h = display.plothorzline(Vmean); set(h,'linestyle','-')
+    %if n==1; title(['Vth = ',num2str(dataVth1{i}(ii(k))), 'mV']); end
+    ylim([-70 -15]); xlim([10 30]);
+    xlabel('Time, sec'); ylabel('V_M, mV')
+    set(gca, 'Fontsize',14,'FontName','Arial'); box off
+    h=display.plothorzline(data{jj}.Vth(j)); hold on
+    end
+    k=k+1;
+end
+
 %% reproduce Figure 4 C-E
 
 jj=1;
@@ -276,7 +308,7 @@ for j=1:5 % map
     end
 end
 
-%% reproduce supplementaty figure for figure 4
+%% supplementaty figure for figure 4
 
 jj=1;
 gH = data{jj}.gH{1}; gSyn = data{jj}.gSyn{1};
@@ -340,11 +372,12 @@ end
 %% plot example traces
 j=1; %experiment
 i=3; %map
-ii=1; % gH,gSyn combination
+ii=43; % gH,gSyn combination
 x = 1/Fs(j):1/Fs(j):length(data{j}.V{i}{ii}(1,:))/Fs(j);
+figure(2)
 clf, plot(x,data{j}.V{i}{ii}(1,:))
 hold on, plot(x,data{j}.V{i}{ii}(2,:))
-data{j}.Vth(i)
+hold on, display.plothorzline(data{j}.Vth(i))
 data{j}.gH{i}(ii)
 data{j}.gSyn{i}(ii)
 
@@ -352,6 +385,18 @@ data{j}.gSyn{i}(ii)
 meanBrstDur{j}{i}(:,ii)
 meanBurDur{j}{i}(:,ii)
 
+%% divide into escape, release and mixed based on the ERQ threhold of -0.038 for escape and 0.105 for release
+regime=[];
+regime1=NaN(7,7);
+for i=1:5 % experiment
+    for j=1:numel(data{i}.V) % map
+        regime1(ERQmean{i}{j}<-0.038) = 1; % escape
+        regime1(ERQmean{i}{j}>-0.038 & ERQmean{i}{j}<0.105) = 2; % mixed
+        regime1(ERQmean{i}{j}>0.105) = 3; % release
+        regime1(isnan(ERQmean{i}{j})) = 0;
+        regime{i}{j}=regime1;
+    end
+end
 
 %% plot state, asymmerty in burst duration and ireggularity in cycle frequencu
 
@@ -362,12 +407,12 @@ g=0.04; l=0.07;
 clf,
 
 for j = 1:5 % map
-    for i=1:3 % number of features to display
+    for i=1:4 % number of features to display
         
         active1 = reshape(meanBurDur{jj}{j}(1,:),[7,7]);
         active2 = reshape(meanBurDur{jj}{j}(2,:),[7,7]);
         
-        h1=display.bigsubplot(3,5,i,j,g,l);
+        h1=display.bigsubplot(4,5,i,j,g,l);
         
         switch i
             
@@ -375,26 +420,64 @@ for j = 1:5 % map
                 display.genimagesc(state{jj}{j}',gH,gSyn);
                 myColorMap = [0.3,0.33,0.35; 0.25,0.4,0.3; 0,0.4,0.5; 0.8,0.5,0.3; 0.6,0.2,0;];
                 colormap(h1,myColorMap);
+                if j==5;
                 h=colorbar;
                 h.Ticks=[1:5]; h.TickLabels={'silent','asymmetric','irregular\newlinespiking','antiphase\newlinespiking','HCO'}; caxis([1 5]);
+                end
                 ylabel('gH');
                 axis square
                 title(['Vth=', num2str(data{jj}.Vth(j)),'mV'])
                 
-            case 2
+%             case 2
+%                 display.genimagesc(ERQ1{jj}{j}{1}',gH,gSyn);
+%                 myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(h1,myColorMap);
+%                 if j == numel(data{jj}.V); h=colorbar; ylabel(h,'ERQ'); end
+%                 caxis([minERQ-0.05 maxERQ+0.02])
+%                 ylabel('gH');
+%                 axis square
+%                 
+%             case 3
+%                 display.genimagesc(ERQ1{jj}{j}{2}',gH,gSyn);
+%                 myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(h1,myColorMap);
+%                 if j == numel(data{jj}.V); h=colorbar; ylabel(h,'ERQ'); end
+%                 caxis([minERQ-0.05 maxERQ+0.02])
+%                 
+%             case 4 % difference in ERQ
+%                 display.genimagesc(abs(ERQ1{jj}{j}{2}-ERQ1{jj}{j}{1})',gH,gSyn);
+%                 myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(h1,myColorMap);
+%                 if j == numel(data{jj}.V); h=colorbar; ylabel(h,'ERQ'); end
+%                caxis([0 0.1])                
+                            
+            case 2 % classified mechanism
+                display.genimagesc(regime{jj}{j},gH,gSyn);
+                myColorMap = [1,1,1; 0.36,0.23,0.6; 0 0.54 0.54; 0.8,0.3,0;];
+                colormap(h1,myColorMap);
+                if j ==5;
+                h=colorbar;
+                h.Ticks=[0:3]; h.TickLabels={'none','escape','mixed','release'};
+                end
+                 caxis([0 3]);
+                ylabel('gH');
+                axis square
+                
+            case 3
                 display.genimagesc(abs(active1-active2)',gH,gSyn);
                 myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(h1,myColorMap);
                 colormap(h1,myColorMap);
                 if j ==5; h=colorbar; ylabel(h,'Asymmerty in burst duration, s'); end
                 caxis([-0.1 3])
+                ylabel('gH');
+                axis square
                 
-            case 3
+            case 4
                 display.genimagesc(CVFRmean{jj}{j},gH,gSyn);
                 myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(h1,myColorMap);
                 if j ==5; h=colorbar; ylabel(h,'CV of cycle frequency'); end
                 %caxis([minCVFR-0.01 maxCVFR+0.01])
                 caxis([minCVFR-0.01 0.2])
                 title(['Vth=',num2str(data{jj}.Vth(j))])
+                ylabel('gH');
+                axis square
         end
         
         xticks([150,300,450,600,750,900,1050]); yticks([150,300,450,600,750,900,1050]);
@@ -420,25 +503,134 @@ set(gca,'xtick',[1:1:5])
 set(gca,'xticklabel',data{jj}.Vth)
 ylabel('Synaptic threhold, mV')
 
-%%
-i=8; %experiment
+%% plot state maps from all the experiments
 
-clf
+gH = data{1}.gH{1}; gSyn = data{1}.gSyn{1};
 
-for i=1:8
-    subtightplot(2,4,i,g,l,l)
-    
-    j=1; %gmi
-    active{i,j}(active{i,j}==0)=NaN;
-    %plot(data.Vth{i}{j}, abs(active{i,j}(:,2) - active{i,j}(:,1)),'.-','linewidth',1)
-    plot(data.Vth{i}{j}, abs(meanBrstDur{i,j}(:,2) - meanBrstDur{i,j}(:,1)),'.-','linewidth',1)
-    
-    j=2;
-    active{i,j}(active{i,j}==0)=NaN;
-    %hold on, plot(data.Vth{i}{j}, abs(active{i,j}(:,2) - active{i,j}(:,1)),'.-','linewidth',1)
-    hold on, plot(data.Vth{i}{j}, abs(meanBrstDur{i,j}(:,2) - meanBrstDur{i,j}(:,1)),'.-','linewidth',1)
-    %hold on, plot(data.Vth{i}{j},active{i,j}(:,2),'.-','linewidth',1)
-   % ylim([0 35])
-    xlim([-53 -33]); xticks([-54:2:-34])
-    legend('ctrl','gmi=150nS')
+g=0.04; l=0.07;
+
+clf,
+
+for i=1:5 % experiment
+    for j=1:numel(data{i}.V) % map
+        h1=display.bigsubplot(5,5,i,j,g,l);
+        
+        display.genimagesc(state{i}{j}',gH,gSyn);
+        myColorMap = [0.3,0.33,0.35; 0.25,0.4,0.3; 0,0.4,0.5; 0.8,0.5,0.3; 0.6,0.2,0;];
+        colormap(h1,myColorMap);
+        h=colorbar;
+        h.Ticks=[1:5]; h.TickLabels={'silent','asymmetric','irregular\newlinespiking','antiphase\newlinespiking','HCO'}; caxis([1 5]);
+        ylabel('gH');
+        axis square
+        title(['Vth=', num2str(data{i}.Vth(j)),'mV'])
+        
+        xticks([150,300,450,600,750,900,1050]); yticks([150,300,450,600,750,900,1050]);
+        xlabel('gSyn'); axis square
+        set(gca,'Fontsize',10,'FontName','Arial');
+        
+        for ii=1:10 % create grid
+            line([0 1200], [150*ii+75 150*ii+75],'color',[0.5 0.5 0.5])
+            line([150*ii+75 150*ii+75],[0 1200],'color',[0.5 0.5 0.5])
+        end
+    end
 end
+
+%% plot ERQ maps for all the experiments
+
+gH = data{1}.gH{1}; gSyn = data{1}.gSyn{1};
+
+g=0.04; l=0.07;
+
+clf,
+
+for i=1:5 % experiment
+    for j=1:numel(data{i}.V) % map
+        h1=display.bigsubplot(5,6,i,j,g,l);
+        
+        display.genimagesc(ERQmean{i}{j},gH,gSyn);
+        myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(h1,myColorMap);
+        if j == numel(data{i}.V); h=colorbar; ylabel(h,'ERQ'); end
+        caxis([minERQ-0.02 maxERQ+0.02])
+        title(['Vth=',num2str(data{i}.Vth(j))])
+        
+        xticks([150,300,450,600,750,900,1050]); yticks([150,300,450,600,750,900,1050]);
+        xlabel('gSyn'); axis square
+        set(gca,'Fontsize',10,'FontName','Arial');
+        
+        for ii=1:10 % create grid
+            line([0 1200], [150*ii+75 150*ii+75],'color',[0.5 0.5 0.5])
+            line([150*ii+75 150*ii+75],[0 1200],'color',[0.5 0.5 0.5])
+        end
+    end
+end
+
+    
+%% plot regime: escape, mixed or release
+
+gH = data{1}.gH{1}; gSyn = data{1}.gSyn{1};
+
+g=0.04; l=0.07;
+
+clf,
+
+for i=1:5 % experiment
+    for j=1:numel(data{i}.V) % map
+        h1=display.bigsubplot(5,6,i,j,g,l);
+        
+        display.genimagesc(regime{i}{j},gH,gSyn);
+     myColorMap = [1,1,1; 0.36,0.23,0.6; 0 0.54 0.54; 0.8,0.3,0;];
+        colormap(h1,myColorMap);
+        h=colorbar;
+        h.Ticks=[0:3]; h.TickLabels={'none','escape','mixed','release'}; caxis([0 3]);
+        ylabel('gH');
+        axis square
+        title(['Vth=', num2str(data{i}.Vth(j)),'mV'])
+        
+        xticks([150,300,450,600,750,900,1050]); yticks([150,300,450,600,750,900,1050]);
+        xlabel('gSyn'); axis square
+        set(gca,'Fontsize',10,'FontName','Arial');
+        
+        for ii=1:10 % create grid
+            line([0 1200], [150*ii+75 150*ii+75],'color',[0.5 0.5 0.5])
+            line([150*ii+75 150*ii+75],[0 1200],'color',[0.5 0.5 0.5])
+        end
+    end
+end
+
+
+%% plot asymmetry maps for all the experiments
+
+gH = data{1}.gH{1}; gSyn = data{1}.gSyn{1};
+
+g=0.04; l=0.07;
+
+clf,
+
+for i=1:5 % experiment
+    for j=1:numel(data{i}.V) % map
+        h1=display.bigsubplot(5,5,i,j,g,l);
+        
+        active1 = reshape(meanBurDur{i}{j}(1,:),[7,7]);
+        active2 = reshape(meanBurDur{i}{j}(2,:),[7,7]);
+        
+        
+        display.genimagesc(abs(active1-active2)',gH,gSyn);
+        myColorMap = display.linspecer(128); myColorMap(1,:) = 1; colormap(h1,myColorMap);
+        colormap(h1,myColorMap);
+        h=colorbar; ylabel(h,'Asymmerty in burst duration, s');
+        caxis([-0.1 3])
+        ylabel('gH');
+        axis square
+        title(['Vth=', num2str(data{i}.Vth(j)),'mV'])
+        
+        xticks([150,300,450,600,750,900,1050]); yticks([150,300,450,600,750,900,1050]);
+        xlabel('gSyn'); axis square
+        set(gca,'Fontsize',10,'FontName','Arial');
+        
+        for ii=1:10 % create grid
+            line([0 1200], [150*ii+75 150*ii+75],'color',[0.5 0.5 0.5])
+            line([150*ii+75 150*ii+75],[0 1200],'color',[0.5 0.5 0.5])
+        end
+    end
+end
+
